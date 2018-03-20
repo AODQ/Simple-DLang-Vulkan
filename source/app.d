cd6cc8ab9519b5befbef168f559c0f3c2b4363f4
aodq <aodq@gmail.com>
2018-03-20 10:19:37 -0400

    Changed to SDL2 dependency

--- source/app.d
+++ source/app.d
@@ -8,37 +8,32 @@ import std.conv;
 import std.string;
 
 import erupted;
-import derelict.glfw3.glfw3;
 import vector;
 import gfm.math;
-
-
+import derelict.sdl2.sdl;
 
 private void enforceVK(VkResult res) {
   enforce(res == VkResult.VK_SUCCESS, res.to!string);
 }
+
 void main ( ) {
   DerelictErupted.load();
-  DerelictGLFW3.load();
-  glfwInit();
+  DerelictSDL2.load();
+  auto sdl_window = SDL_CreateWindow("Vulkan", 0, 0, 800, 600, 0);
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  glfwWindowHint(GLFW_FLOATING, VK_TRUE);
-  glfwWindowHint(GLFW_RESIZABLE, VK_TRUE);
-  glfwWindowHint(GLFW_REFRESH_RATE, VK_FALSE);
-  glfwSwapInterval(1);
-  auto window = glfwCreateWindow(800, 600, "Vulkan window", null, null);
   uint extension_count;
   vkEnumerateInstanceExtensionProperties(null, &extension_count, null);
   writeln("extensions: ", extension_count);
   float4x4 mat;
   float4 vec;
   auto test = mat*vec;
-  while ( !glfwWindowShouldClose(window) ) {
-    glfwPollEvents();
-  }
 
-  glfwDestroyWindow(window);
-  glfwTerminate();
+  SDL_Event event;
+  while ( true ) {
+    while ( SDL_PollEvent(&event) ) {
+      if ( event.type is SDL_QUIT ) goto END;
+    }
+  }
+END:
 }
 
